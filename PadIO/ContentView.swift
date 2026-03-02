2bd5f8a9c35ed9ee9f985401936a004bb6692663
Vincent Grégoire <vincent.gregoire@gmail.com>
2026-03-02 14:53:20 -0500

    Initial Commit

---
+++ PadIO/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PadIO
+//
+//  Created by Vincent Grégoire on 2026-03-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PadIO/PadIOApp.swift
@@ -0,0 +1,17 @@
+//
+//  PadIOApp.swift
+//  PadIO
+//
+//  Created by Vincent Grégoire on 2026-03-02.
+//
+
+import SwiftUI
+
+@main
+struct PadIOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
